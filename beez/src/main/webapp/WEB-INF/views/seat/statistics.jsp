1d0fa9e00eaa8fc2634994c49b6e2a30002ee292
hyeonjae <alskdiel@gmail.com>
2017-10-24 16:28:15 +0900

    modifying seat_statistics (clientside)

--- beez/src/main/webapp/WEB-INF/views/seat/statistics.jsp
+++ beez/src/main/webapp/WEB-INF/views/seat/statistics.jsp
@@ -19,7 +19,72 @@
 			<!-- Content Header (Page header) -->
 			<section class="content-header">
 			
-			좌석통계	
+				
+				<ul class="nav nav-tabs">
+					<li class="active">
+						<a href="#fav" data-toggle="tab">FAVORITE</a>
+					</li>
+					<li>
+						<a href="#hot" data-toggle="tab">HOT PLACE</a>
+					</li>
+				</ul>
+				<div class="tab-content carousel slide" id="carousel-example-generic">
+					<div class="carousel-inner">
+						<div class="tab-pane active item" id="fav">
+							<div class="filter-container">
+								<div class="year fav active">
+									최근 1년
+								</div>
+								<div class="month fav">
+									최근 1개월
+								</div>
+							</div>
+						<!-- 
+							<div class="datepicker-container">
+								<div class="datepicker-wrapper">
+            						<input type='text' class="form-control" id='datepicker-f-f' placeholder="mm/dd/yyyy"/>
+								</div>
+								<div class="datepicker-wrapper">
+									<input type='text' class="form-control" id='datepicker-f-t' placeholder="mm/dd/yyyy" />
+								</div>
+								<div id="icon-search" class="icon-search fav">
+									<i class="fa fa-search" aria-hidden="true"></i>
+								</div>
+							</div>
+						-->
+							<canvas id="favChart" width="400" height="400"></canvas>
+							
+						</div>
+			
+						<div class="tab-pane item" id="hot">
+							<div class="filter-container">
+								<div class="year hot active">
+									최근 1년
+								</div>
+								<div class="month hot">
+									최근 1개월
+								</div>
+							</div>
+						<!-- 
+							<div class="datepicker-container">
+								<div class="datepicker-wrapper">
+            						<input type='text' class="form-control" id='datepicker-h-f' placeholder="mm/dd/yyyy"/>
+								</div>
+								<div class="datepicker-wrapper">
+									<input type='text' class="form-control" id='datepicker-h-t' placeholder="mm/dd/yyyy" />
+								</div>
+								<div id="icon-search-hot" class="icon-search hot">
+									<i class="fa fa-search" aria-hidden="true"></i>
+								</div>
+							</div>
+						-->
+						
+							<canvas id="hotChart" width="400" height="400"></canvas>
+						</div>
+					</div>
+				</div>
+				
+		
 				
 			</section>
 
@@ -27,8 +92,19 @@
 		<div class='control-sidebar-bg'></div>
 	</div>
 	
+	<link href="/resources/css/seat_statistics.css" rel="stylesheet" type="text/css" />
 	
 	<%@include file="./../include/footer.jsp"%>
+	
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.min.css" rel="stylesheet" type="text/css" />
+	
+	<script src="/resources/js/lib/moment.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.js"></script>
+	
+	
+	<script src="/resources/js/lib/Chart.min.js"></script>
+	<script src="/resources/js/seat_statistics.js"></script>  
+	
 	<script type="text/javascript">
 
 
---
+++ beez/src/main/webapp/resources/js/seat_statistics.js
@@ -0,0 +1,191 @@
+var prev = {};
+var current = {};
+var flag = false;
+var isFirstPage = true;
+
+$(".carousel").bind('touchstart', function(e) {
+	if(event.touches[0].pageX > 40) {
+		flag = true;
+		prev.x = event.touches[0].pageX;
+	}
+	
+});
+
+$(".carousel").bind('touchmove', function(e) {
+	if(flag) {
+		var event = e.originalEvent;
+		current.x = event.touches[0].pageX;			
+	}
+});
+
+$(".carousel").bind('touchend', function(e) {
+	var $element = $(this).parent().children(".nav-tabs");
+	
+	if(flag) {
+		if(current.x - prev.x < -40 && isFirstPage) {
+			$(this).carousel('next');
+			isFirstPage = false;
+			toggleNavTabs($element, 'hot');
+		} else if(current.x - prev.x > 40 && !isFirstPage) {
+			$(this).carousel('prev');
+			isFirstPage = true;
+			toggleNavTabs($element, 'fav');
+		}
+	}
+	flag = false;
+});
+
+
+
+
+$(".nav.nav-tabs li").on("click", function() {
+	var $tab = $(this);
+	
+	if($tab.text() == 'FAVORITE') {
+		isFirstPage = true;
+	} else {
+		isFirstPage = false;
+	}
+});
+
+
+function toggleNavTabs($element, type) {	
+	if(type == 'fav') {
+		$element.children("li:nth-child(1)").addClass("active");
+		$element.children("li:nth-child(2)").removeClass("active");
+	} else {
+		$element.children("li:nth-child(2)").addClass("active");
+		$element.children("li:nth-child(1)").removeClass("active");
+	}
+}
+
+$(".filter-container .year").on("click", function() {
+	var $this = $(this);
+	var type;
+	if($this.hasClass("fav")) {
+		type = "fav";
+	} else {
+		type = "hot";
+	}
+	$this.parent().find(".month").removeClass("active");
+	$this.addClass("active");
+});
+
+$(".filter-container .month").on("click", function() {
+	var $this = $(this);
+	var type;
+	if($this.hasClass("fav")) {
+		type = "fav";
+	} else {
+		type = "hot";
+	}
+	$this.parent().find(".year").removeClass("active");
+	$this.addClass("active");
+});
+
+
+/*
+$('#datepicker-f-f').datepicker();
+$('#datepicker-f-t').datepicker();
+$('#datepicker-h-f').datepicker();
+$('#datepicker-h-t').datepicker();
+*/
+/*
+$(".icon-search").on("click", function() {
+	var type;
+	var from;
+	var to;
+	
+	if($(this).hasClass("fav")) {
+		type = "fav";
+		from = $(this).parent().find("#datepicker-f-f").val();
+		to = $(this).parent().find("#datepicker-f-t").val();
+	} else {
+		type = "hot";
+		from = $(this).parent().find("#datepicker-h-f").val();
+		to = $(this).parent().find("#datepicker-h-t").val();	
+	}
+	
+	if(!from || !to) {
+		alert("날짜를 선택해주세요.");
+	} else {
+		console.log(type);
+		console.log(from);
+		console.log(to);
+	}
+});
+*/
+
+
+$(document).on("ready", function() {
+	setChart();
+	
+	function setChart() {
+		setFavChart();
+		setHotChart();
+	}
+	
+	function setFavChart() {
+		var ctx_f = document.getElementById("favChart").getContext('2d');
+		
+		var favChart = new Chart(ctx_f,{
+		    type: 'pie',
+		    data: {
+		    	datasets: [{
+			        data: [10, 20, 30]
+			    }],
+
+			    // These labels appear in the legend and in the tooltips when hovering different arcs
+			    labels: [
+			        'Red',
+			        'Yellow',
+			        'Blue'
+			    ]
+		    }
+		    //options: options
+		});
+	}
+	
+	function setHotChart() {
+		var ctx_h = document.getElementById("hotChart").getContext('2d');
+
+		var hotChart = new Chart(ctx_h, {
+		    type: 'bar',
+		    data: {
+		        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+		        datasets: [{
+		            label: '# of Votes',
+		            data: [12, 19, 3, 5, 2, 3],
+		            backgroundColor: [
+		                'rgba(255, 99, 132, 0.2)',
+		                'rgba(54, 162, 235, 0.2)',
+		                'rgba(255, 206, 86, 0.2)',
+		                'rgba(75, 192, 192, 0.2)',
+		                'rgba(153, 102, 255, 0.2)',
+		                'rgba(255, 159, 64, 0.2)'
+		            ],
+		            borderColor: [
+		                'rgba(255,99,132,1)',
+		                'rgba(54, 162, 235, 1)',
+		                'rgba(255, 206, 86, 1)',
+		                'rgba(75, 192, 192, 1)',
+		                'rgba(153, 102, 255, 1)',
+		                'rgba(255, 159, 64, 1)'
+		            ],
+		            borderWidth: 1
+		        }]
+		    },
+		    options: {
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero:true
+		                }
+		            }]
+		        }
+		    }
+		});
+	}
+	
+
+});
